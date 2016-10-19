709cd7f846cffdaad86bf8402fc27ebd224a12f4
John Fahey <jfahey@diligent.com>
2016-10-20 07:10:21 +1300

    initial commit

---
+++ TestLibrary/AssertionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Emerlahn.Utilities;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Emerlahn.TestLibrary
+{
+    public static class AssertionExtensions
+    {
+        public static void AssertAll(this IEnumerable<Action> asserts)
+        {
+            var exceptions = new List<Exception>();
+            foreach (var assert in asserts)
+            {
+                try
+                {
+                    assert();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            if (exceptions.Any())
+            {
+                if (exceptions.Count == 1) throw exceptions.First();
+                string message = $"Multiple assertions failed:\r\n\r\n{string.Join("\r\n\r\n", exceptions.Select(e => e.Message))}\r\n";
+                throw new Exception(message);
+            }
+        }
+
+        public static void AssertContainsSameValuesAs(this ICollection<string> left, ICollection<string> right, string becauseOnlyOnLeft = null, string becauseOnlyOnRight = null)
+        {
+            var comparer = new SetComparer<string>();
+            var comparison = comparer.Compare(left, right, StringComparer.Ordinal);
+
+            var asserts = new List<Action> {
+                () => comparison.OnlyOnLeft.Should().BeEmpty(becauseOnlyOnLeft),
+                () => comparison.OnlyOnRight.Should().BeEmpty(becauseOnlyOnRight)
+            };
+
+            asserts.AssertAll();
+        }
+
+        public static void VerifyConstructorGuardClauses(this Type type)
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var deferred = new DeferredBehaviourExpectation();
+            var assertion = new GuardClauseAssertion(fixture, deferred);
+            assertion.Verify(type.GetConstructors());
+            deferred.Assertions.AssertAll();
+        }
+    }
+}
---
+++ TestLibrary/DeferredBehaviourExpectation.cs
@@ -0,0 +1,21 @@
+﻿using Ploeh.AutoFixture.Idioms;
+using System;
+using System.Collections.Generic;
+
+namespace Emerlahn.TestLibrary
+{
+    internal class DeferredBehaviourExpectation : IBehaviorExpectation
+    {
+        private readonly IBehaviorExpectation nullAndEmptyGuidBehaviorExpectation;
+        public IList<Action> Assertions { get; } = new List<Action>();
+        public DeferredBehaviourExpectation()
+        {
+            nullAndEmptyGuidBehaviorExpectation = new CompositeBehaviorExpectation(new NullReferenceBehaviorExpectation(), new EmptyGuidBehaviorExpectation());
+        }
+
+        public void Verify(IGuardClauseCommand command)
+        {
+            Assertions.Add(() => nullAndEmptyGuidBehaviorExpectation.Verify(command));
+        }
+    }
+}
