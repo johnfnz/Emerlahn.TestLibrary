3f760ded99518dcfb9bcf5b6eb552d6aa5682061
John <johnfnz@outlook.com>
2016-11-08 20:13:43 +1300

    Tests for VerifyConstructorGuardClauses

---
+++ TestLibraryTests/VerifyConstructorGuardClausesTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using Emerlahn.TestLibrary;
+using FluentAssertions;
+
+namespace TestLibraryTests
+{
+    [TestFixture]
+    public class VerifyConstructorGuardClausesTests
+    {
+        internal class TestClassWithoutGuardClauses
+        {
+            public TestClassWithoutGuardClauses(StringComparer stringComparer, string dogs)
+            {
+            }
+        }
+
+        internal class TestClassWithGuardClauses
+        {
+            public TestClassWithGuardClauses(StringComparer stringComparer, string dogs)
+            {
+                if (stringComparer == null) throw new ArgumentNullException(nameof(stringComparer));
+                if (dogs == null) throw new ArgumentNullException(nameof(dogs));
+            }
+        }
+
+        [Test]
+        public void Given_constructor_without_guard_clauses_should_throw()
+        {
+            Action act = () => typeof(TestClassWithoutGuardClauses).VerifyConstructorGuardClauses();
+
+            act.ShouldThrow<Exception>();
+        }
+
+        [Test]
+        public void Given_constructor_with_guard_clauses_shouldnt_throw()
+        {
+            Action act = () => typeof(TestClassWithGuardClauses).VerifyConstructorGuardClauses();
+
+            act.ShouldNotThrow<Exception>();
+        }
+    }
+}
